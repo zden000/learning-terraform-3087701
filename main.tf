2cc9f5bd3d7d9dd5375c663c4fe4e32006e439ac
zden000 <60679712+zden000@users.noreply.github.com>
2022-12-13 09:28:49 +1300

    Create security group

--- main.tf
+++ main.tf
@@ -14,10 +14,10 @@ data "aws_ami" "app_ami" {
   owners   = ["979382823631"] # Bitnami
 }
 
-
 data "aws_vpc" "default" {
   default = true
 }
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
